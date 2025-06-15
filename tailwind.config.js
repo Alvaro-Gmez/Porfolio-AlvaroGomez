92f0adc62aa29c8bdccc24c1ba7e5a478b4be665
4lvaroo <alvarogd548@gmail.com>
2025-06-15 04:40:05 +0200

    Social Pills, pendiente de revisar funcionamiento

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 }
 
