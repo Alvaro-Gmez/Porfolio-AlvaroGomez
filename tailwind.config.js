4310a9cfedaefa66cc764320a50d4550dcf41268
agomezEVG <agomezdelgado.guadalupe@alumnado.fundacionloyola.net>
2025-09-16 18:20:33 +0200

    Preparacion de proyectos, ajuste de tamaños

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        wave: {
+          '0%': { transform: 'rotate(0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10deg)' },
+          '60%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+        pulseGrow: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.05)' },
+        }
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+        wave: 'wave 2s ease-in-out infinite',
+        pulseGrow: 'pulseGrow 2s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
