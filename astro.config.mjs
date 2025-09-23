9928cbb8303db9f7ac93f048c974b1abcf1c2849
Alvaro-Gmez <alvarogd548@gmail.com>
2025-09-23 14:20:27 +0200

    Prueba de despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://4lvaroo.github.io/portfolio/',
-  base: '/portfolio/',
+  site: 'https://github.com/Alvaro-Gmez/Porfolio-AlvaroGomez',
+  base: '/',
   integrations: [tailwind()]
 });
