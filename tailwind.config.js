85c975f7716925d6dcc49224fa592013607cf5ef
4lvaroo <alvarogd548@gmail.com>
2025-09-27 01:49:44 +0200

    Correcciones post despliegue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+import { custom } from 'astro:schema';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        custom: "var(--font-family)",
+      },
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
